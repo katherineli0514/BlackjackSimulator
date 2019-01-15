96aa19915489bb814cf760979aa6d779ac6f5572
Katherine Li <katherineli0514@gmail.com>
2019-01-14 21:44:32 -0800

    Create initial project.

---
+++ BlackjackSimulator/BlackjackSimulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlackjackSimulator
+//
+//  Created by Katherine Li on 1/14/19.
+//  Copyright © 2019 Katherine Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
